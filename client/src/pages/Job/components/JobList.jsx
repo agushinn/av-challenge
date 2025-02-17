8480421c230ae44a31219d83a1d5d171d621c178
Agustin Reyes <areyes@MacBook-Pro-2.local>
2025-02-17 13:11:08 -0300

    add order functionality

--- client/src/pages/Job/components/JobList.jsx
+++ client/src/pages/Job/components/JobList.jsx
@@ -1,15 +1,76 @@
+import { useState } from 'react'
+
 import styles from '@styles/pages/Job/components/JobList.module.scss'
 import { currencyFormat } from '@utils/currencyFormat'
 import { Link } from 'react-router-dom'
 
+const SORT_TYPES = {
+    TITLE: 'TITLE',
+    SALARY: 'SALARY',
+}
+
+const SORT_ORDER = {
+    ASC: 'ASC',
+    DESC: 'DESC',
+}
+
 const JobList = ({ jobs, className, canEdit }) => {
+    const [sortType, setSortType] = useState(SORT_TYPES.TITLE)
+    const [sort, setSort] = useState(SORT_ORDER.ASC)
+
+    const handleSortType = (e) => {
+        setSortType(e.target.value)
+    }
+
+    const handleSort = (e) => {
+        setSort(e.target.value)
+    }
+
+    const sortedJobs = () => {
+        let sortedJobs = [...jobs]
+        if (sortType === SORT_TYPES.TITLE) {
+            sortedJobs.sort((a, b) => {
+                if (sort === SORT_ORDER.ASC) {
+                    return a.title.localeCompare(b.title)
+                }
+                return b.title.localeCompare(a.title)
+            })
+        }
+
+        if (sortType === SORT_TYPES.SALARY) {
+            sortedJobs.sort((a, b) => {
+                if (sort === SORT_ORDER.ASC) {
+                    return a.salary - b.salary
+                }
+                return b.salary - a.salary
+            })
+        }
+        return sortedJobs
+    }
+
     return (
         <div className={className}>
+            <div className={styles.sortContainer}>
+                <select name="sortType" id="sortType" onChange={handleSortType}>
+                    <option value="TITLE">Title</option>
+                    <option value="SALARY">Salary</option>
+                </select>
+                <select name="sort" id="sort" onChange={handleSort}>
+                    <option value="ASC">Ascending</option>
+                    <option value="DESC">Descending</option>
+                </select>
+            </div>
+            <div className={styles.sortInfo}>
+                <span>Sort by: </span>
+                <span>{sortType}</span>
+                <span> - </span>
+                <span>{sort}</span>
+            </div>
             <h5 className={styles.jobsListTitle}>
                 Result quantity: {jobs.length}
             </h5>
             <ul className={styles.jobsList}>
-                {jobs.map((job) => (
+                {sortedJobs().map((job) => (
                     <li key={job.id} className={` ${styles.jobItem}`}>
                         <h2>{job.title}</h2>#{job.id}
                         <p>
